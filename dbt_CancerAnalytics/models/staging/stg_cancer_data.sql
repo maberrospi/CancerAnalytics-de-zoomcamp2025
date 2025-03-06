1dfb8b95d6849f62b7d82145f9caf638da9dc96f
Michael Angelo Berrospi <blckmessiah@protonmail.com>
2025-03-06 13:43:00 +0100

    Create the dbt project

---
+++ dbt_CancerAnalytics/models/staging/stg_cancer_data.sql
@@ -0,0 +1,7 @@
+-- with tripdata as 
+-- (
+--   select *,
+--     row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+--   from {{ source('staging','external_green_data') }}
+--   where vendorid is not null 
+-- )
